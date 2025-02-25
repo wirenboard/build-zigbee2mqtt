813418b487fc4bbf68bace3a8499a0fc8d200d13
BurchuMikhail <141926144+BurchuMikhail@users.noreply.github.com>
2025-02-25 09:23:50 +0300

    add installation of pnpm dependencies when upgrading a package

--- package/after-upgrade.sh
+++ package/after-upgrade.sh
@@ -2,6 +2,9 @@
 
 CONFIG_FILE=/mnt/data/root/zigbee2mqtt/data/configuration.yaml
 
+echo "Adding dependencies for pnpm"
+pnpm install --frozen-lockfile --prefix /mnt/data/root/zigbee2mqtt
+
 if [ -e "$CONFIG_FILE.wb-old" ]; then
     echo "Restoring config file after upgrade from old malformed zigbee2mqtt package version"
     mv $CONFIG_FILE.wb-old $CONFIG_FILE
