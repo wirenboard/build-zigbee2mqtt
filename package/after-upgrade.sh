0393236401ba31fafb4b0d55b1b687a3c142453b
BurchuMikhail <141926144+BurchuMikhail@users.noreply.github.com>
2025-02-26 10:38:29 +0300

    Fix configuration file (#16)
    * add pnpm install via corepack when updating package
    
    * add installation of pnpm dependencies when upgrading a package
    
    * add zstack adapter type to the configuration file
    
    * add the default zstack adapter type to the configuration file

--- package/after-upgrade.sh
+++ package/after-upgrade.sh
@@ -9,3 +9,19 @@ if [ -e "$CONFIG_FILE.wb-old" ]; then
     echo "Restoring config file after upgrade from old malformed zigbee2mqtt package version"
     mv $CONFIG_FILE.wb-old $CONFIG_FILE
 fi
+
+if ! grep -Pzq 'serial:\n(  .*\n)*  adapter: zstack' $CONFIG_FILE; then
+  LINE=$(awk '
+    /^serial:/ { inside=1; next }
+    inside && /^[^ ]/ { exit }
+    inside { last_line = NR }
+    END { print last_line }
+  ' "$CONFIG_FILE")
+
+  if [ -n "$LINE" ]; then
+    sed -i "${LINE}a \  adapter: zstack" $CONFIG_FILE
+  else
+    sed -i "/^serial:/a \  adapter: zstack" $CONFIG_FILE
+  fi
+  echo "zstack adapter type added to the configuration file"
+fi
